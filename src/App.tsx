65649479587f79f77c8c0f57a61ef8f0edf8e80f
Andrew R <beta@runelite.bot>
2024-08-27 16:24:33 -0500

    show theme selection always

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,14 @@ function App() {
   return (
     <main className="min-h-screen h-screen w-full flex flex-col bg-background gap-2">
       <div className="flex flex-col items-center h-full p-8">
-        <h1 className="text-4xl font-bold text-text duration-300 text-center p-8">
+        <h1 className="text-4xl font-bold text-text duration-300 text-center p-4">
           All demo, no bullshit
         </h1>
         <Widget />
-        {!isConnected && (
-          <div className="flex mt-12 items-center justify-center space-x-4">
-            <ThemeSelect />
-            <ModeToggle />
-          </div>
-        )}
+        <div className="flex mt-6 items-center justify-center space-x-4">
+          <ThemeSelect />
+          <ModeToggle />
+        </div>
       </div>
     </main>
   );
