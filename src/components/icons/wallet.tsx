efe8a8cbedaf5282c2bf1c74119772f4763904fb
Andrew R <beta@runelite.bot>
2024-08-27 13:42:28 -0500

    feat: add icon component

---
+++ src/components/icons/wallet.tsx
@@ -0,0 +1,36 @@
+/// <reference types="vite-plugin-svgr/client" />
+import React from "react";
+
+// we need to import the svg files directly to prevent all svgs from being bundled with the app
+import MetamaskSVG from "@web3icons/core/svgs/wallets/branded/metamask.svg?react";
+import CoinbaseSVG from "@web3icons/core/svgs/wallets/branded/coinbase.svg?react";
+import WalletConnectSVG from "@web3icons/core/svgs/wallets/branded/wallet-connect.svg?react";
+import TrustWalletSVG from "@web3icons/core/svgs/wallets/branded/trust.svg?react";
+import SafeSVG from "@web3icons/core/svgs/wallets/branded/safe.svg?react";
+
+import { CubeIcon } from "@radix-ui/react-icons";
+
+interface WalletIconProps {
+  iconName: string;
+}
+
+const walletIcons: Record<string, React.FC> = {
+  metamask: MetamaskSVG,
+  coinbase: CoinbaseSVG,
+  coinbaseWalletSDK: CoinbaseSVG,
+  walletconnect: WalletConnectSVG,
+  trustwallet: TrustWalletSVG,
+  safe: SafeSVG,
+};
+
+const WalletIcon: React.FC<WalletIconProps> = ({ iconName }) => {
+  const IconComponent = walletIcons[iconName as keyof typeof walletIcons];
+
+  if (!IconComponent) {
+    return <CubeIcon className="w-6 h-6" />;
+  }
+
+  return <IconComponent />;
+};
+
+export default WalletIcon;
