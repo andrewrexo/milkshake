9e3860e8ae91d5f5bf62e13a54eae59fdb0fb60f
Andrew R <beta@runelite.bot>
2024-08-27 13:43:19 -0500

    remove coinbaseWallet import
    increased bundle size by ~100kb, probably not worth it for now

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,10 +1,10 @@
 import { http, createConfig } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
-import { coinbaseWallet, injected, safe } from "wagmi/connectors";
+import { injected, safe } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
-  connectors: [injected(), coinbaseWallet(), safe()],
+  connectors: [injected(), safe()],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
