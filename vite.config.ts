721d1271dae4c80239cda81af7bbd3cd142944c7
Andrew R <beta@runelite.bot>
2024-08-29 13:16:48 -0500

    output solana web3 in separate chunk

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    svgr({
-      svgrOptions: {
-        // svgr options
+    svgr(),
+    nodePolyfills({
+      include: ["crypto", "stream", "path", "util"],
+      globals: {
+        Buffer: true,
+        global: true,
       },
     }),
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "solana-web3": ["@solana/web3.js"],
+        },
+      },
+    },
+  },
 });
