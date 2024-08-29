74461783277cb0b6c6d2690e726cbf104378878b
Andrew R <beta@runelite.bot>
2024-08-29 13:52:44 -0500

    rename app to App

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { WagmiProvider } from "wagmi";
 
-import App from "./app.tsx";
+import App from "./App.tsx";
 import { config } from "./wagmi.ts";
 
 import "./styles/index.css";
