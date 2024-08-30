daca680289e3118e19598ddba70fd3d9da134503
Andrew R <beta@runelite.bot>
2024-08-30 10:32:51 -0500

    disable deeplink for metamask mobile

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,7 +2,7 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl } from "@solana/web3.js";
 import { http, createConfig } from "wagmi";
 import { arbitrum, base, mainnet, optimism } from "wagmi/chains";
-import { injected, safe, metaMask } from "wagmi/connectors";
+import { injected, metaMask } from "wagmi/connectors";
 
 export const config = createConfig({
   chains: [mainnet, arbitrum, base, optimism],
@@ -11,8 +11,9 @@ export const config = createConfig({
     injected({
       unstable_shimAsyncInject: 2_000,
     }),
-    metaMask(),
-    safe(),
+    metaMask({
+      useDeeplink: false,
+    }),
   ],
   transports: {
     [mainnet.id]: http(),
